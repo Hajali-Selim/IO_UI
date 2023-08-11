12f0d718defd3ae30318c18517cc6cd9d1af4778
Selim <32914199+Hajali-Selim@users.noreply.github.com>
2023-08-11 17:52:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,11 +55,12 @@ for c in countries:
 sector_groups = ['Agriculture', 'Extraction and mining', 'Manufacture', 'Utilities', 'Services']
 sector_groupmap = {'Agriculture':[0], 'Extraction and mining':list(range(1,16)), 'Manufacture':list(range(16,48)), 'Utilities':list(range(48,64)), 'Services':list(range(64,72))}
 data_y = {'standard':{int(c[0]):c[1] for c in H.groupby(['year'])[units_list+metrics_list]}, 'yearly variation':{int(c[0]):c[1] for c in Hv.groupby(['year'])[units_list+metrics_list]}}
-height, width = {'country':17, 'sector':16.5, 'region':16.5}, {'country':NY*25, 'sector':NY*30, 'region':NY*75}
+height, width = {'country':17, 'sector':16.5, 'region':16.5}, {'country':27*25, 'sector':27*30, 'region':27*75}
 
-app.layout = html.Div([
+app.layout = html.Div(children=[
     dcc.Markdown('''# visualizing vulnerability''', style={'textAlign':'center'}),
-    dcc.Markdown('11/08/2023 (13:30) update: addition of \'waves\' figure'),
+    html.Div('11/08/2023 (13:30) update: addition of \'waves\' figure'),
+    html.Div('11/08/2023 (15:30) update: new colormap and corrected forward linkages'),
     html.Hr(),
     dbc.Accordion([
     	dbc.AccordionItem(
@@ -76,7 +77,7 @@ app.layout = html.Div([
     			html.Tr([html.Td(dcc.Markdown('**select unit**', style={'textAlign':'right'})),
      				html.Td(dcc.Dropdown(units_list, 'country', id='unit1v'))]),
      			html.Tr([html.Td(dcc.Markdown('**select year**', style={'textAlign':'right'})),
-    				html.Td(dcc.Dropdown(list(range(1995,2023)), 2000, id='year1v'))]), ]), borderless=True), width=4),
+    				html.Td(dcc.Dropdown(list(range(1995,2021)), 2000, id='year1v'))]), ]), borderless=True), width=4),
     		dbc.Col(#column of ordering selection
     		[dbc.Col(dcc.Markdown('**select ordering**', style={'textAlign':'center'}), width=2),
     		dbc.Col(dcc.RadioItems(ordering_list, 'original', id='order1v', inputStyle={'margin-right':'10px', 'margin-left':'30px'}), width=6)]),
@@ -95,7 +96,7 @@ app.layout = html.Div([
     			html.Tr([html.Td(dcc.Markdown('**select unit**', style={'textAlign':'right'})),
      				html.Td(dcc.Dropdown([units_list[2]], 'sector', id='unit1s'))]),
      			html.Tr([html.Td(dcc.Markdown('**select year**', style={'textAlign':'right'})),
-    				html.Td(dcc.Dropdown(list(range(1995,2023)), 2000, id='year1s'))]), ]), borderless=True), width=4),
+    				html.Td(dcc.Dropdown(list(range(1995,2021)), 2000, id='year1s'))]), ]), borderless=True), width=4),
     		dbc.Col(#column of ordering selection
     		[dbc.Col(dcc.Markdown('**select ordering**', style={'textAlign':'center'}), width=2),
     		dbc.Col(dcc.RadioItems(ordering_list, 'original', id='order1s', inputStyle={'margin-right':'10px', 'margin-left':'30px'}), width=6)]),]),
@@ -112,7 +113,7 @@ app.layout = html.Div([
     				html.Tr([html.Td(dcc.Markdown('**select unit**', style={'textAlign':'right'})),
      					html.Td(dcc.Dropdown([units_list[2]], 'sector', id='unit1m'))]),
      				html.Tr([html.Td(dcc.Markdown('**select year**', style={'textAlign':'right'})),
-    					html.Td(dcc.Dropdown(list(range(1995,2023)), 2000, id='year1m'))]), ]), borderless=True), width=4),
+    					html.Td(dcc.Dropdown(list(range(1995,2021)), 2000, id='year1m'))]), ]), borderless=True), width=4),
 		dbc.Col([dbc.Col(dcc.Markdown('**select ordering**', style={'textAlign':'center'}), width=2),
     			dbc.Col(dcc.RadioItems(ordering_list, 'original', id='order1m', inputStyle={'margin-right':'10px', 'margin-left':'30px'}), width=4)]),]),
     		dcc.Graph(figure={}, id='hist1m'), ], label='Monetary importance', activeTabClassName='fw-bold'),
@@ -162,7 +163,6 @@ app.layout = html.Div([
     			, label='Sectors', activeTabClassName='fw-bold')])
     			, title='Heatmaps'),
     	
-    	
     	dbc.AccordionItem(
     		dbc.Tabs([dbc.Tab(dbc.Tabs([
     				dbc.Tab([html.Br(),
@@ -205,17 +205,42 @@ app.layout = html.Div([
     		dbc.Row([dbc.Col(dcc.Markdown('**select country**', style={'textAlign':'right'}), width=2),
     			dbc.Col(dcc.Dropdown(countries, 'Australia', id='unit4'), width=2)]),
     		dbc.Row([dbc.Col(dcc.Markdown('**select year**', style={'textAlign':'right'}), width=2),
-    			dbc.Col(dcc.Slider(1995, 2022, 1, marks={y:str(y) for y in range(1995,2023)}, updatemode='drag', value=1996, id='year4'))]),
-    		#dbc.Row([dbc.Col(dcc.Markdown('**rescale marker size**', style={'textAlign':'right'}), width=2),
-    		#	dbc.Col(dcc.Slider(1,3,.5, updatemode='drag',value=1, id='marker_size4'), width=2)]),
-    		dbc.Row(dcc.Graph(figure={}, id='bubble4')), ], title='Bubble plots'),
+    			dbc.Col(dcc.Slider(1995, 2020, 1, marks={y:str(y) for y in range(1995,2021)}, updatemode='drag', value=1996, id='year4'))]),
+    		dbc.Row(dcc.Graph(figure={}, id='bubble4'))]
+    		, title='Bubble plots'),
     	
     	dbc.AccordionItem([
     		dbc.Row([dbc.Col(dcc.Markdown('**select country**', style={'textAlign':'right'}), width=2),
 			dbc.Col(dcc.Dropdown(countries_list, 'Germany', id='unit5'), width=2)]),
-    		dbc.Row(dcc.Graph(figure={}, id='waves5'))
-    		
-    			], title='Waves'),
+    		dbc.Row(dcc.Graph(figure={}, id='waves5'))]
+    		, title='Waves'),
+    	
+    	dbc.AccordionItem(
+    		dbc.Tabs([dbc.Tab([
+    			dbc.Row([dbc.Col(dcc.Markdown('**select year**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(range(1995,2021), '2000', id='year6_c'))]),
+    			dbc.Row([dbc.Col(dcc.Markdown('**select metric1 (vulnerability as node color)**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(vulnerability_list, 'gas', id='metric6_c'), width=2)],),
+    			dbc.Row([dbc.Col(dcc.Markdown('**select metric2 (node size)**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(['out-degree (export)', 'in-degree (import)', 'betweenness'], 'betweenness', id='nsize6_c'))]),
+    			dbc.Row([dbc.Col(dcc.Markdown('**select metric2 (edge size)**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(['Monetary importance', 'Structural importance'], 'Monetary importance', id='esize6_c'))]),
+    			dbc.Row(dcc.Graph(figure={}, id='network6_c'))],
+    			label='Countries', activeTabClassName='fw-bold'),
+    			
+    			dbc.Tab([
+    			dbc.Row([dbc.Col(dcc.Markdown('**select year**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(range(1995,2021), '2000', id='year6_s'))]),
+    			dbc.Row([dbc.Col(dcc.Markdown('**select metric1 (vulnerability as node color)**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(vulnerability_list, 'gas', id='metric6_s'), width=2)],),
+    			dbc.Row([dbc.Col(dcc.Markdown('**select metric2 (node size)**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(['out-degree (export)', 'in-degree (import)', 'betweenness'], 'betweenness', id='nsize6_s'))]),
+    			dbc.Row([dbc.Col(dcc.Markdown('**select metric2 (edge size)**', style={'textAlign':'right'}), width=2),
+    				dbc.Col(dcc.Dropdown(['Monetary importance', 'Structural importance'], 'Monetary importance', id='esize6_s'))]),
+    			dbc.Row(dcc.Graph(figure={}, id='network6_s'))],
+    			label='Sectors', activeTabClassName='fw-bold')],)
+    		, title='Network view')
+    			
 			], flush=True)
 			])
 
@@ -240,7 +265,7 @@ def update_cheatmap(metric2_c,order2_c,unit2_c):
     order = (order2_c=='original')*'trace' + (order2_c=='descending')*'total descending' + (order2_c=='ascending')*'total ascending'
     cinds = [country_to_idx[c] for c in unit2_c]
     data_c = deepcopy(data_cavg.iloc[sorted([k*NC+i for k in range(NY) for i in cinds])])
-    return px.density_heatmap(data_c, x='year', y='country', z=metric2_c, height=height['country']*len(cinds), width=width['country'], labels={'x':'year','y':'country'}, nbinsx=NY, nbinsy=len(unit2_c)).update_xaxes(dtick=3, ticklen=10, tickwidth=3, ticks='outside').update_yaxes(tickmode='linear', ticklen=7, tickwidth=2, ticks='outside', autorange='reversed', categoryorder=order).update_layout(font={'size':15}, showlegend=True, coloraxis_colorbar={'title':'vulnerability (%)'})
+    return px.density_heatmap(data_c, x='year', y='country', z=metric2_c, height=height['country']*len(cinds), width=width['country'], labels={'x':'year','y':'country'}, nbinsx=NY, nbinsy=len(unit2_c), color_continuous_scale='Turbo').update_xaxes(dtick=3, ticklen=10, tickwidth=3, ticks='outside').update_yaxes(tickmode='linear', ticklen=7, tickwidth=2, ticks='outside', autorange='reversed', categoryorder=order).update_layout(font={'size':15}, showlegend=True, coloraxis_colorbar={'title':'vulnerability (%)'})
 
 @callback(Output('heatmap2_s1', 'figure'), [Input(s, 'value') for s in ['metric2_s1','group2_s1', 'order2_s1','unit2_s1']])
 def update_s1heatmap(metric2_s1,group2_s1,order2_s1,unit2_s1):
@@ -248,7 +273,7 @@ def update_s1heatmap(metric2_s1,group2_s1,order2_s1,unit2_s1):
     g1inds = sum([sector_groupmap[g] for g in group2_s1],[])
     s1inds = list(set([sector_to_idx[s] for s in unit2_s1]) & set(g1inds))
     data_s1 = deepcopy(data_s1avg.iloc[sorted([k*NS+i for k in range(NY) for i in s1inds])])
-    return px.density_heatmap(data_s1, x='year', y='sector', z=metric2_s1, height=height['sector']*len(s1inds), width=width['sector'], labels={'x':'year','y':'sector'}, nbinsx=NY, nbinsy=len(s1inds)).update_xaxes(dtick=3, ticklen=10, tickwidth=3, ticks='outside').update_yaxes(tickmode='linear', ticklen=7, tickwidth=2, ticks='outside', autorange='reversed', categoryorder=order).update_layout(font={'size':15}, showlegend=True, coloraxis_colorbar={'title':'vulnerability (%)'})
+    return px.density_heatmap(data_s1, x='year', y='sector', z=metric2_s1, height=height['sector']*len(s1inds), width=width['sector'], labels={'x':'year','y':'sector'}, nbinsx=NY, nbinsy=len(s1inds), color_continuous_scale='Jet').update_xaxes(dtick=3, ticklen=10, tickwidth=3, ticks='outside').update_yaxes(tickmode='linear', ticklen=7, tickwidth=2, ticks='outside', autorange='reversed', categoryorder=order).update_layout(font={'size':15}, showlegend=True, coloraxis_colorbar={'title':'vulnerability (%)'})
 
 @callback(Output('heatmap2_s2', 'figure'), [Input(s, 'value') for s in ['metric2_s2','group2_s2','order2_s2','unit2_s2']])
 def update_s2heatmap(metric2_s2,group2_s2,order2_s2,unit2_s2):
@@ -260,7 +285,7 @@ def update_s2heatmap(metric2_s2,group2_s2,order2_s2,unit2_s2):
     for r_idx in range(len(regions_list)):
         r = regions_list[r_idx]
         fig.add_trace(px.density_heatmap(data_s2[r], x='year', y='sector', z=metric2_s2, nbinsx=NY, nbinsy=len(s2inds)).data[0], row=1, col=r_idx+1)
-    fig.update_xaxes(dtick=3, ticklen=10, tickwidth=3, ticks='outside').update_yaxes(tickmode='linear', ticklen=7, tickwidth=2, ticks='outside', autorange='reversed', categoryorder=order).update_layout(height=height['region']*len(s2inds), width=width['region'], font={'size':15}, showlegend=True, coloraxis_colorbar={'title':'vulnerability (%)'})
+    fig.update_xaxes(dtick=3, ticklen=10, tickwidth=3, ticks='outside').update_yaxes(tickmode='linear', ticklen=7, tickwidth=2, ticks='outside', autorange='reversed', categoryorder=order).update_layout(height=height['region']*len(s2inds), width=width['region'], font={'size':15}, showlegend=True, coloraxis_colorbar={'title':'vulnerability (%)'}, coloraxis={'colorscale':'Jet'})
     return fig
 
 @callback(Output('lines3_c1', 'figure'), [Input(s, 'value') for s in ['metric3_c1','region3_c1']])
@@ -292,14 +317,14 @@ def update_bubble(metric4t,metric4v,unit4,year4):
     country_dataset, zlabel = H[H['country']==unit4], metric4v
     xmin, xmax, ymin, ymax, zmin, zmax = country_dataset[xlabel].min(), country_dataset[xlabel].max(), country_dataset[ylabel].min(), country_dataset[ylabel].max(), country_dataset[zlabel].min(), country_dataset[zlabel].max()
     dataset = country_dataset[country_dataset['year']==year4]
-    fig = px.scatter(dataset, x=xlabel, y=ylabel, size=zlabel, color=zlabel, labels={'x':xlabel, 'y':ylabel}, range_x=[xmin-.1,xmax+.1], range_y=[ymin-.1,ymax+.1], range_color=[zmin-.02, zmax+.02], hover_name='sector', symbol='transition', opacity=.8).update_layout(width=1200, height=800, font={'size':20}, coloraxis_colorbar={'title':'vulnerability (%)'}, hoverlabel={'font_size':18}, legend={'orientation':'h', 'yanchor':'bottom', 'y':1.02, 'entrywidth':200, 'title':None}).update_traces(marker_line_width=dataset['abate']*3, marker_line_color='red')
+    fig = px.scatter(dataset, x=xlabel, y=ylabel, size=zlabel, color=zlabel, labels={'x':xlabel, 'y':ylabel}, range_x=[xmin-.1,xmax+.1], range_y=[ymin-.1,ymax+.1], range_color=[zmin-.02, zmax+.02], hover_name='sector', symbol='transition', opacity=.8, color_continuous_scale='Jet').update_layout(width=1200, height=800, font={'size':20}, coloraxis_colorbar={'title':'vulnerability (%)'}, hoverlabel={'font_size':18}, legend={'orientation':'h', 'yanchor':'bottom', 'y':1.02, 'entrywidth':200, 'title':None}).update_traces(marker_line_width=dataset['abate']*3, marker_line_color='red')
     for i,j in enumerate(legend_names):
         fig.data[i].name=legend_names[j]
     if metric4t == 'Monetary importance':
         fig.add_hline(y=1, line_width=3, line_dash='dash', line_color='red', opacity=.7).add_vline(x=1, line_width=3, line_dash='dash', line_color='red', opacity=.7)
     return fig
 
-@callback(Output('waves5', 'figure'), [Input(s, 'value') for s in ['unit5']])
+@callback(Output('waves5', 'figure'), Input('unit5', 'value'))
 def update_waves(unit5):
     fig = px.area(data_cvuln[unit5], x='year', y='vulnerability', color='sector')
     return fig
@@ -324,7 +349,6 @@ def toggle_s2canvas(n, is_open):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
 
 
 
